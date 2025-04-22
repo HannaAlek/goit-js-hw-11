f82b1891a3e8cce935b281dcbf2e0ab88ec947dc
HannaAlek <aniuta_alex@ukr.net>
2025-04-22 16:52:00 +0300

    update render

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-axios({
-    method: 'get',
-    url: 'https://jsonplaceholder.typicode.com/users'
-  })
-.then(response => console.log(response))
-	.catch(error => console.log(error));
